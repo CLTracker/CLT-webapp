49c5b17a08cd766dce72a83949b4ec78d6ad087a
Grant Mercer <Gmercer015@gmail.com>
2017-03-21 11:02:45 -0700

    finish skeleton join page implementation

--- frontend/src/app/+join/join.component.ts
+++ frontend/src/app/+join/join.component.ts
@@ -1,5 +1,7 @@
 import { Component }        from '@angular/core';
-import { Router }                   from '@angular/router';
+import { Router }           from '@angular/router';
+import { AuthHttp }         from 'angular2-jwt';
+import { Auth }             from '../shared';
 
 @Component({
     selector: 'my-join-page',
@@ -8,5 +10,33 @@ import { Router }                   from '@angular/router';
 })
 
 export class JoinComponent {
-    constructor() {}
+    private org: String = "";
+    constructor(private auth: Auth, 
+        private authHttp: AuthHttp,
+        private router: Router) { }
+
+    joinOrg(): void {
+        let headers: any = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+
+        let data: any = JSON.stringify({
+            user_metadata: {
+                organization: this.org
+            }
+        });
+
+        this.authHttp
+            .patch('https://' + 'clt-global.auth0.com' + '/api/v2/users/' + this.auth.userProfile.user_id, data, {headers: headers})
+            .map(response => response.json())
+            .subscribe(
+                response => {
+                    this.auth.userProfile = response;
+                    localStorage.setItem('profile', JSON.stringify(response));
+                    this.router.navigate(['profile']);
+                },
+                error => alert(error.json().message)
+            );
+    }
 }
--- frontend/src/app/+join/join.module.ts
+++ frontend/src/app/+join/join.module.ts
@@ -1,22 +1,26 @@
 import { NgModule }                 from '@angular/core';
 import { CommonModule }             from '@angular/common';
 import { HttpModule }               from '@angular/http';
-import { AuthGuard }                from './../shared/auth-guard.service';
+import { MaterialModule }           from '@angular/material';
+import { FormsModule }              from '@angular/forms';
 import { JoinRoutingModule }     from './join.routing';
 import { JoinComponent }         from './join.component';
+import { AUTH_PROVIDERS }        from 'angular2-jwt';
 
 @NgModule({
     imports: [
         CommonModule,
         HttpModule,
-        JoinRoutingModule
+        JoinRoutingModule,
+        FormsModule,
+        MaterialModule
     ],
     exports: [],
     declarations: [
         JoinComponent
     ],
     providers: [
-        AuthGuard
+        AUTH_PROVIDERS
     ]
 })
 export class JoinModule { }
