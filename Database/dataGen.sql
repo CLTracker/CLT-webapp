13476c83503f4049fdd76e642c9e8ed9f517a516
Steven Brooks <stevenbrookslv@gmail.com>
2017-03-29 02:12:53 +0000

    Started a data generation file for mysql

---
+++ Database/dataGen.sql
@@ -0,0 +1,5 @@
+#THIS FILE WILL BE FOR ALL SAMPLE DATA GENERATION
+
+INSERT INTO permsissions("permission_name") VALUES("xhb");
+INSERT INTO permsissions("permission_name") VALUES("org");
+INSERT INTO permsissions("permission_name") VALUES("adm");
