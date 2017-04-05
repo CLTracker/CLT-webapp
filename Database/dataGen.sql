9112a245055065df5e235e3d9ac394e6b3fda263
Jose Arjee Mendez <arjee.mendez@gmail.com>
2017-04-04 20:41:48 -0700

    dataGen fix

--- Database/dataGen.sql
+++ Database/dataGen.sql
@@ -12,7 +12,7 @@ INSERT INTO permissions(permission_name) VALUES("org");
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(1, "Steven Brooks", 0, "2017-03-28 00:00:00", "0.0.0.0", "brooks@unlv.nevada.edu", 1, 2);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(2, "Lily Lei", 0, "2017-03-28 00:00:00", "0.0.0.0", "leil1@unlv.nevada.edu", 0, 2);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(3, "Arjee M", 0, "2017-11-03 00:00:00", "0.0.0.0", "mendej2@unlv.nevada.edu", 1, 2);
-INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(4, "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "xhb1@email.com", 0, 1);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(4, "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb1@email.com", 0, 1);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(5, "Org1", 0, "2017-03-28 00:00:00", "0.0.0.0", "org1@email.com", 0, 3);
 
 INSERT INTO permitted_organizers(organizer_email) VALUES("org1@email.com");
