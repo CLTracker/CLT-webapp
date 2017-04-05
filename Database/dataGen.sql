e3f758342c6b246c07c24f7df5115bc500cbf74a
Steven Brooks <brooks@unlv.nevada.edu>
2017-04-04 18:35:45 -0700

    Sample data matches database structure

--- Database/dataGen.sql
+++ Database/dataGen.sql
@@ -9,14 +9,14 @@ INSERT INTO permissions(permission_name) VALUES("org");
 
 
 
-INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(1, "Steven Brooks", 0, "2017-03-28 00:00:00", "0.0.0.0", "brooks@unlv.nevada.edu", 1, 2);
-INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(2, "Lily Lei", 0, "2017-03-28 00:00:00", "0.0.0.0", "leil1@unlv.nevada.edu", 0, 2);
-INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(3, "Arjee M", 0, "2017-11-03 00:00:00", "0.0.0.0", "mendej2@unlv.nevada.edu", 1, 2);
-INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(4, "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhib1@unlv.nevada.edu", 0, 1);
-INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(5, "Org1", 0, "2017-03-28 00:00:00", "0.0.0.0", "org@unlv.nevada.edu", 0, 3);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(1, "Steven Brooks", 0, "2017-03-28 00:00:00", "0.0.0.0", "brooks@unlv.nevada.edu", 1, 2);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(2, "Lily Lei", 0, "2017-03-28 00:00:00", "0.0.0.0", "leil1@unlv.nevada.edu", 0, 2);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(3, "Arjee M", 0, "2017-11-03 00:00:00", "0.0.0.0", "mendej2@unlv.nevada.edu", 1, 2);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(4, "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhib1@unlv.nevada.edu", 0, 1);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES(5, "Org1", 0, "2017-03-28 00:00:00", "0.0.0.0", "org@unlv.nevada.edu", 0, 3);
 
-INSERT INTO permitted_organizers(organizer_id, conference, exhibitor_name, contact_phone, contact_email) VALUES(5, 1, "Org1", "702-555-5555", "org@unlv.nevada.edu");
-INSERT INTO permitted_exhibitors(exhibitor_id, conference, exhibitor_name, contact_phone, contact_email, logo_url) VALUES(4, 1, "Exhib1", "702-555-5555", "exhib1@unlv.nevada.edu", "https://hasdhfkasdjf.com");
+INSERT INTO permitted_organizers(organizer_id) VALUES(5);
+INSERT INTO permitted_exhibitors(exhibitor_id) VALUES(4);
 
-INSERT INTO organizers(organizer_id, conference, organizer_name, contact_phone, contact_email) VALUES(5, 1, "Org1", "702-555-5555", "org@unlv.nevada.edu");
-INSERT INTO exhibitors(exhibitor_id, conference, exhibitor_name, contact_phone, contact_email, logo_url) VALUES(4, 1, "Exhib1", "702-555-5555", "exhib1@unlv.nevada.edu", "https://hasdhfkasdjf.com");
+INSERT INTO organizers(organizer_id, conference) VALUES(5, 1);
+INSERT INTO exhibitors(exhibitor_id, conference, company_name, logo_url) VALUES(4, 1, "Exxon Mobile", "https://hasdhfkasdjf.com");
