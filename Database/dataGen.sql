a10edf015fbcd22b82747576e3b8cbd460a7b070
Steven Brooks <stevenbrookslv@gmail.com>
2017-04-05 01:21:45 +0000

    dataGen.sql update

--- Database/dataGen.sql
+++ Database/dataGen.sql
@@ -1,12 +1,22 @@
 #THIS FILE WILL BE FOR ALL SAMPLE DATA GENERATION
 
 
-INSERT INTO conference(conference_id, floor_plan, conference_name, start_date, end_date, location, icon) VALUES(1, "https://lol.com", "Senior Design Competition", "2017-03-19", "2017-03-20", "4580 Maryland Pkwy", "https://lol2.com");
+INSERT INTO conference(conference_id, floor_plan, conference_name, start_date, end_date, location, logo_url) VALUES(1, "https://lol.com", "Senior Design Competition", "2017-03-19", "2017-03-20", "4580 Maryland Pkwy", "https://lol2.com");
 
 INSERT INTO permissions(permission_name) VALUES("xhb");
 INSERT INTO permissions(permission_name) VALUES("adm");
 INSERT INTO permissions(permission_name) VALUES("org");
 
+
+
 INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(1, "Steven Brooks", 0, "2017-03-28 00:00:00", "0.0.0.0", "brooks@unlv.nevada.edu", 1, 2);
 INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(2, "Lily Lei", 0, "2017-03-28 00:00:00", "0.0.0.0", "leil1@unlv.nevada.edu", 0, 2);
 INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(3, "Arjee M", 0, "2017-11-03 00:00:00", "0.0.0.0", "mendej2@unlv.nevada.edu", 1, 2);
+INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(4, "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhib1@unlv.nevada.edu", 0, 1);
+INSERT INTO users(user_id, username, login_count, last_login, last_ip, email, gender, permissions) VALUES(5, "Org1", 0, "2017-03-28 00:00:00", "0.0.0.0", "org@unlv.nevada.edu", 0, 3);
+
+INSERT INTO permitted_organizers(organizer_id, conference, exhibitor_name, contact_phone, contact_email) VALUES(5, 1, "Org1", "702-555-5555", "org@unlv.nevada.edu");
+INSERT INTO permitted_exhibitors(exhibitor_id, conference, exhibitor_name, contact_phone, contact_email, logo_url) VALUES(4, 1, "Exhib1", "702-555-5555", "exhib1@unlv.nevada.edu", "https://hasdhfkasdjf.com");
+
+INSERT INTO organizers(organizer_id, conference, organizer_name, contact_phone, contact_email) VALUES(5, 1, "Org1", "702-555-5555", "org@unlv.nevada.edu");
+INSERT INTO exhibitors(exhibitor_id, conference, exhibitor_name, contact_phone, contact_email, logo_url) VALUES(4, 1, "Exhib1", "702-555-5555", "exhib1@unlv.nevada.edu", "https://hasdhfkasdjf.com");
