f55d62a6811cc4f1f6d5aae46b09e2c4599289dc
Steven Brooks <brooks@unlv.nevada.edu>
2017-04-18 18:26:51 -0700

    Updating sample get

--- Database/dataGen.sql
+++ Database/dataGen.sql
@@ -13,21 +13,22 @@ INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("2", "Lily Lei", 0, "2017-03-28 00:00:00", "0.0.0.0", "leil1@unlv.nevada.edu", 0, 2);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("3", "Arjee M", 0, "2017-11-03 00:00:00", "0.0.0.0", "mendej2@unlv.nevada.edu", 1, 2);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("4", "Exhib1", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb1@email.com", 0, 1);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("5", "Exhib2", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb2@email.com", 0, 1);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("6", "Exhib3", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb3@email.com", 0, 1);
+INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("7", "Exhib4", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb4@email.com", 0, 1);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("google-oauth2|105950922553210130192", "Grant Organizer", 0, "2017-03-28 00:00:00", "0.0.0.0", "gmercer015@gmail.com", 0, 3);
-INSERT INTO users(email, user_id, name, permissions) VALUES("org2@email.com", "6", "Org2", 3);
-INSERT INTO users(email, user_id, name, permissions) VALUES("org3@email.com", "7", "Org3", 3);
-INSERT INTO users(email, user_id, name, permissions) VALUES("org4@email.com", "8", "Org4", 3);
-INSERT INTO users(email, user_id, name, permissions) VALUES("org5@email.com", "9", "Org5", 3);
-INSERT INTO users(email, user_id, name, permissions) VALUES("xhb2@email.com", "10", "Xhb2", 1);
-INSERT INTO users(email, user_id, name, permissions) VALUES("xhb3@email.com", "11", "Xhb3", 1);
-INSERT INTO users(email, user_id, name, permissions) VALUES("xhb4@email.com", "12", "Xhb4", 1);
-INSERT INTO users(email, user_id, name, permissions) VALUES("xhb5@email.com", "13", "Xhb5", 1);
 
 INSERT INTO permitted_organizers(organizer_email) VALUES("gmercer015@gmail.com");
 INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb1@email.com");
+INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb2@email.com");
+INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb3@email.com");
+INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb4@email.com");
 
 INSERT INTO organizers(organizer_email, conference) VALUES("org2@email.com", 1);
 INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb1@email.com", 1, "Exxon Mobile", "https://hasdhfkasdjf.com");
+INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb2@email.com", 1, "Google", "https://hasdhfkasdjf.com");
+INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb3@email.com", 1, "Steven Mobile", "https://hasdhfkasdjf.com");
+INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb4@email.com", 1, "Cantor inc", "https://hasdhfkasdjf.com");
 
 INSERT INTO schedule(conference, event_name, start_time, end_time) VALUES(1, "EVENT 1", "2017-03-28 00:00:00", "2017-03-28 00:00:00");
 INSERT INTO schedule(conference, event_name, start_time, end_time) VALUES(1, "EVENT 2", "2017-03-28 00:00:00", "2017-03-28 00:00:00");
