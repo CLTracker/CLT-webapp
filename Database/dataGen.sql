9c9cfc080acab8cbbb8c2e156227cc2130de3fe3
Arjee <arjee@goHunt.com>
2017-04-25 18:58:57 -0700

    Update schedule table

--- Database/dataGen.sql
+++ Database/dataGen.sql
@@ -18,14 +18,14 @@ INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("7", "Exhib4", 0, "2017-03-28 00:00:00", "0.0.0.0", "exhb4@email.com", 0, 1);
 INSERT INTO users(user_id, name, login_count, last_login, last_ip, email, gender, permissions) VALUES("google-oauth2|105950922553210130192", "Grant Organizer", 0, "2017-03-28 00:00:00", "0.0.0.0", "gmercer015@gmail.com", 0, 3);
 
-INSERT INTO permitted_organizers(organizer_email, conference_name) VALUES("gmercer015@gmail.com","Senior Design Competition" );
-INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb1@email.com");
-INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb2@email.com");
-INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb3@email.com");
-INSERT INTO permitted_exhibitors(exhibitor_email) VALUES("exhb4@email.com");
+INSERT INTO permitted_organizers(organizer_email, conference_id) VALUES("gmercer015@gmail.com",1 );
+INSERT INTO permitted_exhibitors(exhibitor_email, conference_id) VALUES("exhb1@email.com",1);
+INSERT INTO permitted_exhibitors(exhibitor_email, conference_id) VALUES("exhb2@email.com",1);
+INSERT INTO permitted_exhibitors(exhibitor_email, conference_id) VALUES("exhb3@email.com",1);
+INSERT INTO permitted_exhibitors(exhibitor_email, conference_id) VALUES("exhb4@email.com",1);
 
-INSERT INTO organizers(organizer_email, conference) VALUES("org2@email.com", 1);
-INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb1@email.com", 1, "Exxon Mobile", "https://hasdhfkasdjf.com");
+INSERT INTO organizers(organizer_email, conference) VALUES("gmercer015@gmail.com", 1);
+INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb1@email.com", 1, "Exxon Mobile", "http://i.imgur.com/58x0vtM.jpg");
 INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb2@email.com", 1, "Google", "https://hasdhfkasdjf.com");
 INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb3@email.com", 1, "Steven Mobile", "https://hasdhfkasdjf.com");
 INSERT INTO exhibitors(exhibitor_email, conference, company_name, logo_url) VALUES("exhb4@email.com", 1, "Cantor inc", "https://hasdhfkasdjf.com");
@@ -34,6 +34,6 @@ INSERT INTO schedule(conference, event_name, start_time, end_time) VALUES(1, "EV
 INSERT INTO schedule(conference, event_name, start_time, end_time) VALUES(1, "EVENT 2", "2017-03-28 00:00:00", "2017-03-28 00:00:00");
 INSERT INTO schedule(conference, event_name, start_time, end_time) VALUES(1, "EVENT 3", "2017-03-28 00:00:00", "2017-03-28 00:00:00");
 
-INSERT INTO news(conference, title, logo_url, text) VALUES (1, "EVENT 1", "https://event1.com", "Free donuts for life");
-INSERT INTO news(conference, title, logo_url, text) VALUES (1, "EVENT 2", "https://event2.com", "Free donuts for life");
-INSERT INTO news(conference, title, logo_url, text) VALUES (1, "EVENT 3", "https://event3.com", "Free donuts for life");
+INSERT INTO news(conference, title, logo_url, text, author) VALUES (1, "EVENT 1", "https://event1.com", "Free donuts for life", "Pepe");
+INSERT INTO news(conference, title, logo_url, text, author) VALUES (1, "EVENT 2", "https://event2.com", "Free donuts for life", "Pepe");
+INSERT INTO news(conference, title, logo_url, text, author) VALUES (1, "EVENT 3", "https://event3.com", "Free donuts for life", "Pepe");
