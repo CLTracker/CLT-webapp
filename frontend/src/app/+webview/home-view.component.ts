e4aa68c0d9d8bbe8e35dbf38cbc338d13ca8c409
Syntaf <gmercer015@gmail.com>
2017-05-03 18:28:04 -0700

    Fix sidenav view having blurry text with one hell of a hack

--- frontend/src/app/+webview/home-view.component.ts
+++ frontend/src/app/+webview/home-view.component.ts
@@ -1,4 +1,5 @@
 import { Component }        from '@angular/core';
+import { Router, RouterState }           from '@angular/router';
 
 @Component({
     selector: 'my-home-view',
@@ -7,6 +8,30 @@ import { Component }        from '@angular/core';
 })
 
 export class HomeViewComponent {
-    constructor() {
+    private currentView: string;
+    private currentRoute: string;
+
+    constructor(private router: Router) {
+        console.log(this.router.routerState)
+    }
+
+    navigate(nav: any, route: string) {
+        this.currentRoute = route;
+        switch(route) {
+            case 'news':
+                this.currentView = 'News';
+            break;
+            case 'schedule':
+                this.currentView = 'Schedule';
+            break;
+            case 'map':
+                this.currentView = 'Convention Map';
+            break;
+            case 'exhibitors':
+                this.currentView = 'Exhibitors'
+            break;
+        }
+        nav.close();
+        this.router.navigate([route]);
     }
 }
