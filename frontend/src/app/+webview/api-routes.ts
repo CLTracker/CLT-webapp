fde2e10f23b3f10a3574e38fd010520b5ac14603
Syntaf <gmercer015@gmail.com>
2017-05-03 22:09:32 -0700

    change webview to production routes

--- frontend/src/app/+webview/api-routes.ts
+++ frontend/src/app/+webview/api-routes.ts
@@ -5,16 +5,9 @@ interface routes {
     ScheduleInfoUrl: string
 }
 
-// export const ROUTES: routes = {
-//     ConferenceInfoUrl: 'http://cltglobal.ddns.net:8080/info/1',
-//     ExhibitorsInfoUrl: 'http://cltglobal.ddns.net:8080/user/exhibitors/1',
-//     NewsInfoUrl: 'http://cltglobal.ddns.net:8080/news/1',
-//     ScheduleInfoUrl: 'http://cltglobal.ddns.net:8080/schedule/1'
-// };
-
 export const ROUTES: routes = {
-    ConferenceInfoUrl: 'http://localhost:5000/info/1',
-    ExhibitorsInfoUrl: 'http://localhost:5000/user/exhibitors/1',
-    NewsInfoUrl: 'http://localhost:5000/news/1',
-    ScheduleInfoUrl: 'http://localhost:5000/schedule/1'
+    ConferenceInfoUrl: 'https://clt.website/info/1',
+    ExhibitorsInfoUrl: 'https://clt.website/user/exhibitors/1',
+    NewsInfoUrl: 'https://clt.website/news/1',
+    ScheduleInfoUrl: 'https://clt.website/schedule/1'
 };
